3ba17092475759bf4becf80380b9dd1c112f5384
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-04-21 20:02:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
-let hour, minute, second
-hour = minute = second = 0
-const chrono = document.getElementById("time")
-const symbol = document.getElementById("symbol")
-let started = false
+let segundo = Number(localStorage.getItem("segundosSalvos")) || 0;
+let minuto = Number(localStorage.getItem("minutosSalvos")) || 0;
+let hora = Number(localStorage.getItem("horasSalvos")) || 0;
+let andando = false;
 
-function time() {
-    started = !started;
-    if (symbol.innerHTML === "▶") {
-        symbol.innerHTML = "⏸"
-    } else {
-        symbol.innerHTML = "▶"
+const chronoDisplay = document.getElementById("time");
+const symbolBtn = document.getElementById("symbol");
+
+//Atualiza o tempo total
+const allTimeDisplay = document.getElementById("total-display");
+const h = hora.toString().padStart(2, "0");
+const m = minuto.toString().padStart(2, "0");
+const s = segundo.toString().padStart(2, "0");
+allTimeDisplay.innerHTML = (hora > 0) ? `${h}:${m}:${s}` : `${m}:${s}`;
+
+function toggleTimer() {
+    andando = !andando;
+    symbolBtn.innerHTML = andando ? "⏸" : "▶"; //condição ? valor_se_verdadeiro : valor_se_falso
+
+    if (!andando) {
+        localStorage.setItem("segundosSalvos", segundo);
+        localStorage.setItem("minutosSalvos", minuto);
+        localStorage.setItem("horasSalvos", hora);
     }
 }
 
 setInterval(() => {
-    if (started === true) {
-        second += 1;
-        if (second >= 60) {
-            second -= 60;
-            minute += 1;
-        } if (minute >= 60) {
-            minute -= 60;
-            hour += 1;
+    if (andando) {
+        segundo += 1;
+        if (segundo >= 60) {
+            segundo = 0;
+            minuto += 1;
+        } 
+        if (minuto >= 60) {
+            minuto = 0;
+            hora += 1;
         }
-
-        const h = hour.toString().padStart(2, "0");
-        const m = minute.toString().padStart(2, "0");
-        const s = second.toString().padStart(2, "0");
-
-        if (h === "00") {
-            chrono.innerHTML = `${m}:${s}`;
-        } else {
-        chrono.innerHTML = `${h}:${m}:${s}`;
-        }
+        updateDisplay();
     }
 }, 1000);
 
-function addTime() {
-    second += 60
-    minute += 1
+function updateDisplay() {
+    const h = hora.toString().padStart(2, "0");
+    const m = minuto.toString().padStart(2, "0");
+    const s = segundo.toString().padStart(2, "0");
+
+    // Mostra horas apenas se for maior que 0
+    chronoDisplay.innerHTML = (hora > 0) ? `${h}:${m}:${s}` : `${m}:${s}`;
 }
